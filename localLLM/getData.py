858bb3a688290abec452fb93e7055dd47e5de056
cmb-sy <free.nakashima@gmail.com>
2024-08-24 17:21:33 +0900

    update

--- localLLM/getData.py
+++ localLLM/getData.py
@@ -1,26 +1,29 @@
 import polars as pl
 import numpy as np
 
-data = pl.read_csv('./data/test.csv')
-
-def getData(data):
+def create_dataframe(data):
+    # 名前以外のnull値を含む行を削除
+    not_null_data = data.drop_nulls(subset=data[:, 1:].columns)
+    
     one_dimensional_vector = []
-    # 行ごとにデータを取得
-    for row in data.iter_rows():
+    for row in not_null_data.iter_rows():
+        # Noneでないデータをクリーンアップ
         cleaned_data = [item for item in row if item is not None]
+        # データをカンマで連結
         concatenated_row = ','.join(map(str, cleaned_data))
         one_dimensional_vector.append(concatenated_row)
-    # NumPy配列に変換し、(193, 1)の形状にリシェイプ
+    
+    # 1次元のベクトルをnumpy配列に変換し、1列の2次元配列に変形
     one_dimensional_array = np.array(one_dimensional_vector).reshape(-1, 1)
-    return one_dimensional_array
+    
+    # 新しいDataFrameを作成
+    new_df = pl.DataFrame({
+        "名前": not_null_data[:, 0],  # 元のデータの最初の列を名前として使用
+        'キーワード': one_dimensional_array  # 連結されたデータをキーワードとして使用
+    })
+    return new_df
 
 if __name__ == "__main__":
-    # 名前以外の全ての値がnullの列を削除
-    notNullData = data.drop_nulls(subset=data[:,1:].columns)
-    result = getData(notNullData[:, 1:])
-    new_df = pl.DataFrame({
-        "名前": notNullData[:, 0],
-        'キーワード': result
-    })
-    
+    data = pl.read_csv('./data/test.csv')
+    new_df = create_dataframe(data)
     print(new_df)
--- localLLM/ollama.py
+++ localLLM/ollama.py
@@ -1,4 +1,5 @@
 import ollama
+
 stream = ollama.chat(
     model="gemma2:2b",
         messages=[{"role": "user", "content": "趣味が筋トレの人の1週間の過ごし方を1文で教えて？"}],
@@ -6,3 +7,6 @@ stream = ollama.chat(
 )
 for chunk in stream:
     print(chunk["message"]["content"], end="", flush=True)
+
+
+"以下のキーワードから構成されるパーソナリティを考えてください。次にその人物の1週間の生活ストーリをA4一枚程度の文字数で作成してください。"
